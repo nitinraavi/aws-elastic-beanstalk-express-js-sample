28e3cfacdf19ee0044127ad23f656e21ec4b5281
Nitin Raavi <Nitin.Raavi@ATMECS.CORP>
2021-03-14 22:48:40 +0530

    Full PipeLine Deployment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 80;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World! This is Full Pipelined Deployment'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
